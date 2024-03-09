302044c9bc236df7cc239e82684b2501691746ee
George007361 <gera.illarionov@mail.ru>
2024-03-10 00:44:58 +0300

    SmokeText done

--- src/components/base/SmokeText.tsx
+++ src/components/base/SmokeText.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
+import mask from '@/utils/mask';
 
-export default ({children}) => {
-	return children;
+export default ({ children }) => {
+	if (typeof children !== 'string') {
+		return children;
+	}
+	
+	return mask(children);
 };
