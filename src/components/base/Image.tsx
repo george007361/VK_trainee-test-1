1d55e602574fd5630a3917d6eb7a0b6d5084563d
George007361 <gera.illarionov@mail.ru>
2024-03-09 14:08:31 +0300

    t1: Image.tsx - uuid+style+div

--- src/components/base/Image.tsx
+++ src/components/base/Image.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 
+// Создать строку длиной 10 из 9 случайных a-z и 0-9
+const genClassName = (() => {
+	// Алфавит
+	const chars = [];
+	for (let i = 0; i < 9; ++i) {
+		// Случайные a-z символы
+		chars.push(String.fromCharCode(Math.floor(Math.random() * (122 - 97)) + 97));
+		// Цифры
+		chars.push(i);
+	}
+
+	return () => {
+		let uid = chars[0]; // Имя класса начинается на a-z
+		for (let i = 0; i < 9; ++i) {
+			uid += chars[Math.floor(Math.random() * chars.length)];
+		}
+		return uid;
+	}
+})();
+
 export default (props) => {
+	const { src, ...rest } = props;
+	const class_name = genClassName();
+
 	return (
-		<img
-			alt=""
-			{...props}
-		/>
+		<>
+			<style>
+				{`.${class_name}{content: url(${src});}.${class_name}:hover{transform: scale(1.02);}`}
+			</style>
+			<div className={class_name} {...rest} />
+		</>
 	);
 };
