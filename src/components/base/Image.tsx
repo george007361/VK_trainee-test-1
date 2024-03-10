1087f32b06e56ed035af579b5296e3dbb7635757
George007361 <gera.illarionov@mail.ru>
2024-03-10 21:19:20 +0300

    typescript,  move height and width values to css class

--- src/components/base/Image.tsx
+++ src/components/base/Image.tsx
@@ -3,12 +3,12 @@ import * as React from 'react';
 // Создать строку длиной 10 из 9 случайных a-z и 0-9
 const genClassName = (() => {
 	// Алфавит
-	const chars = [];
+	const chars : string[] = [];
 	for (let i = 0; i < 9; ++i) {
 		// Случайные a-z символы
 		chars.push(String.fromCharCode(Math.floor(Math.random() * (122 - 97)) + 97));
 		// Цифры
-		chars.push(i);
+		chars.push(i.toString());
 	}
 
 	return () => {
@@ -20,16 +20,23 @@ const genClassName = (() => {
 	}
 })();
 
-export default (props) => {
-	const { src, ...rest } = props;
-	const class_name = genClassName();
 
+interface IImageProps {
+	src: string,
+	width: number;
+	height: number;
+}
+
+export default (props : IImageProps) => {
+	const { src, width, height} = props;
+	const class_name = genClassName();
+	
 	return (
 		<>
 			<style>
-				{`.${class_name}{content: url(${src});}.${class_name}:hover{transform: scale(1.02);}`}
+				{`.${class_name}{content: url(${src});width:${width}px;height:${height}px;}.${class_name}:hover{transform: scale(1.02);}`} 
 			</style>
-			<div className={class_name} {...rest} />
+			<div className={class_name}/>
 		</>
 	);
 };
